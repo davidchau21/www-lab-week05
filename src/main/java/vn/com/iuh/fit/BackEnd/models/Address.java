fc22503cd6171ba9a75d17b520497d2633628a07
davidchau21 <davidduongxu1@gmail.com>
2023-11-10 22:30:39 +0700

    address

--- src/main/java/vn/com/iuh/fit/BackEnd/models/Address.java
+++ src/main/java/vn/com/iuh/fit/BackEnd/models/Address.java
@@ -2,7 +2,6 @@ package vn.com.iuh.fit.BackEnd.models;
 
 import com.neovisionaries.i18n.CountryCode;
 import jakarta.persistence.*;
-<<<<<<< HEAD
 import lombok.*;
 
 @Entity
@@ -35,44 +34,4 @@ public class Address {
         this.country = country;
     }
 
-=======
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.EqualsAndHashCode;
-import lombok.NoArgsConstructor;
-
-@Entity
-@EqualsAndHashCode(of = {"id"})
-@Table(name = "address")
-@NoArgsConstructor
-@AllArgsConstructor
-@Data
-public class Address {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private long id;
-    @Column(columnDefinition = "varchar(150)")
-    private String street= null;
-    @Column(columnDefinition = "varchar(50)")
-    private String city= null;
-    @Column(columnDefinition = "smallint(6)")
-    private CountryCode country = null;
-    @Column(columnDefinition = "varchar(20)")
-    private String number= null;
-    @Column(columnDefinition = "varchar(7)")
-    private String zipcode= null;
-
-    public Address(CountryCode country) {
-        this.country = country;
-    }
-
-    public Address(String street, String city, CountryCode country, String number, String zipcode) {
-        this.street = street;
-        this.city = city;
-        this.country = country;
-        this.number = number;
-        this.zipcode = zipcode;
-    }
-
->>>>>>> 573571e00878c205a8a4e49aa5f1751dc16145b5
 }
